e5b8f1032faebaf4aea12b5bd6f51070e1869a3c
Jeimmy Gutierrez <yuranigutierrez0710@gmail.com>
2024-08-09 15:50:14 -0500

    php

---
+++ variables/index.php
@@ -0,0 +1,63 @@
+<?php
+
+//EJEMPLO 1
+/*$numero = 5;
+$nombre = 'Sara';
+$altura = 1.58*/
+
+//EJEMPLO 2 
+/*$edad;
+$edad = 20;
+echo $edad;*/
+
+//EJEMPLO 3
+/*echo $x= 9;
+echo $x= 2;
+echo $x= 3;*/ #imprime el numero completo
+
+//EJEMPLO 4
+/*$va = 456;
+echo $va;*/
+
+//EJEMPLO 5
+/*echo $nombre = 'yura';
+echo $nombre = 'jeimmy';*/
+
+//EJEMPLO 6
+/*$this = 'hola';
+echo $this;*/ //esto genera error 
+
+//EJEMPLO 7
+/*$nombre = 'jei';
+$nombre = 'yura';
+
+echo $nombre; */
+
+//EJEMPLO 8
+/*$a = 'Analisis';
+$b = 4;
+ echo $b;*/
+
+//EJEMPLO 9 funcion global
+/*$x=5;
+function prueba(){
+    echo "variable global: " .$x;
+}
+prueba();*/ #ESTE CAMPO DE ERROR
+
+//EJEMPLO 10
+/*function prueba(){
+
+    $x=5;
+    echo "La variable es: $x"; 
+}
+prueba();*/
+
+//EJEMPLO 11 VARIABLE LOCAL
+$edad=21;
+function prueba(){
+    global $edad;
+}
+
+
+?>
